d7518d9b173c698b6bfe76066be68d9e3405831b
Sofia Katzman <katzmansof@gmail.com>
2023-03-11 19:16:35 -0500

    Palindrome solution 1 implemented

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
